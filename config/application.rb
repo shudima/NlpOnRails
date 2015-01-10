c0e41811aebe50418ac87ca1b0878eb999dda29f
Dima Shulga <shudima@gmail.com>
2015-01-11 00:32:10 +0200

    remove db

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +21,6 @@ module NlpOnRails
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+   # config.active_record.raise_in_transactional_callbacks = true
   end
 end
